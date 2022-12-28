f8746da38a4f6c832315088bb04685ea2727b68f
harilal04 <harilalc66@gmail.com>
2022-12-29 00:08:16 +0530

    added email and password check middleware

--- SRC/ROUTES/V1/index.js
+++ SRC/ROUTES/V1/index.js
@@ -1,8 +1,16 @@
 const userController = require('../../CONTROLLER/user-controller');
 const express = require('express');
 const router = express.Router();
+const {AuthRequestValidators} = require('../../MIDDLEWARES/index');
 
-router.post('/signup', userController.create);
-router.post('/signin', userController.signIn);
+router.post('/signup',
+    AuthRequestValidators.validateUserAuth,
+    userController.create
+ );
+
+router.post('/signin',
+    AuthRequestValidators.validateUserAuth,
+    userController.signIn
+);
 
 module.exports = router;
